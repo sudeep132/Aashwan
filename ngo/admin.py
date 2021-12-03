28c38f907b61e8333b85a70f966267edda2befe3
vishal <vdavishu@gmail.com>
2021-12-03 21:06:34 +0530

    :boom: Added index.html

--- ngo/admin.py
+++ ngo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(NGO)
+admin.site.register(Events)
 
 # Register your models here.
--- ngo/models.py
+++ ngo/models.py
@@ -9,9 +9,15 @@ class NGO(models.Model):
     helpline_no=models.CharField(max_length=30)
     description=models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Events(models.Model):
     location=models.TextField()
     venue=models.TextField()
     ngo_id=models.ForeignKey(NGO,on_delete=models.CASCADE,default=None)
     name=models.CharField(max_length=100)
     description=models.TextField()
+
+    def __str__(self):
+        return self.name
