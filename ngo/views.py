73e8832c86e68010ae58bf93e79d8486c7663b61
vishal <vdavishu@gmail.com>
2021-12-03 22:06:26 +0530

    :rocket: Updated ngo/views.py

--- ngo/views.py
+++ ngo/views.py
@@ -1,6 +1,11 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    #ngos=NGO.objects.all()
+    ngos = NGO.objects.raw('select * from NGO')
+    data={'ngos':ngos}
+    print(data)
+    return render(request, 'index.html',data)
