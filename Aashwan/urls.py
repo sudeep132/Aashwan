7fcc34dc94d8802fa3b8bdbedfa4aea865f5e0dd
vishal <vdavishu@gmail.com>
2021-12-06 23:04:52 +0530

    :zap: Added form for creating volunteer

--- Aashwan/urls.py
+++ Aashwan/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('ngo.urls')),
-    path('events/',include('event.urls'))
+    path('events/',include('event.urls')),
+    path('volunteer/',include('volunteer.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- volunteer/views.py
+++ volunteer/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import redirect, render
-
+from django.views.decorators.csrf import csrf_exempt
 from volunteer.models import Volunteers
+from django.db import connection
+from event.models import Event
 
 # Create your views here.
+cursor=connection.cursor()
+@csrf_exempt
 def create_volunteer(request):
     if(request.method=="POST"):
         name=request.POST['name']
         email_id=request.POST['email']
         phone_no=request.POST['phone']
         gender=request.POST['gender']
-        volunteer=Volunteers.objects.raw('''INSERT INTO volunteer_volunteers (name,email_id,phone_no,gender) VALUES ('{}','{}','{}','{}');'''.format(name,email_id,phone_no,gender))
+        event_id=Event.objects.get(pk=1)
+        print(event_id)
+        query='''INSERT INTO volunteer_volunteers (name,email_id,phone_no,gender,event_id_id,credit) VALUES ('{}','{}','{}','{}','{}','{}');'''.format(name,email_id,phone_no,gender,1,0)
+        cursor.execute(query)
+        #volunteer=Volunteers.objects.raw('''INSERT INTO volunteer_volunteers (name,email_id,phone_no,gender,event_id_id,credit) VALUES ('{}','{}','{}','{}','{}','{}');'''.format(name,email_id,phone_no,gender,0,0))
         return redirect('/')
